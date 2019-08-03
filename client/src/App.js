32fcff99102e6ccd70e676562b9e8c1b3a3c8fea
Faraah <ibarra.faraah@gmail.com>
2019-08-03 10:38:11 -0500

    Template for home/about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./components/Nav";
+import Parallax from "./components/Parallax";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Team Bunnies</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Parallax />
+
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/components/Parallax/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./style.css";
+
+function Parallax() {
+    return (
+        <>
+            <div class="img1">
+                <div class="ptext">
+                    <span class="border">
+                        Odyssey Chicago
+                </span>
+                </div>
+            </div>
+            <section class="section section-light">
+                <h2> About <hr></hr> </h2>
+                <p>Welcome to Chiology! Doesn't matter where you're coming from or where you're going, our mission is to help you find adventures in Chi-town! <br></br>
+                    Not sure what foods to try? Where to explore? Or the best neighborhoods to hang out at? Well here on our website you have access to all that information.
+                    </p> 
+            </section>
+            <div class="img2">
+                <div class="ptext">
+                    <span class="">
+                    </span>
+                </div>
+            </div>
+            <section class="section section-dark">
+                <h2> Our Services </h2>
+                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores odit incidunt minima recusandae beatae
+                    iure reprehenderit dolores, voluptas itaque nemo, facilis excepturi dicta vitae dolorem, ipsam reiciendis
+            dolore explicabo quis.</p>
+            </section>
+
+            <div class="img3">
+                <div class="ptext">
+                    <span class="">
+                    </span>
+                </div>
+            </div>
+
+
+
+        </>
+
+    );
+}
+
+export default Parallax;

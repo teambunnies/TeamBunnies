015e487e7d9db71a2a2dc3d0168607ca70d9943d
aafzal <ambreenfafzal@gmail.com>
2019-08-14 18:52:25 -0500

    Updated changes

--- client/src/components/Parallax/index.js
+++ client/src/components/Parallax/index.js
@@ -8,14 +8,13 @@ function Parallax() {
                 <div class="ptext">
                     <span class="border">
                         Discover Chicago 
-                </span>
-                <button type="button" class="btn btn-outline-dark"> Login | Create Account</button>
+                     </span>
                 </div>
             </div>
             <section class="section section-light">
-                <h2> About <hr></hr> </h2>
-                <p>Welcome to Chiology! Doesn't matter where you're coming from or where you're going, our mission is to help you find adventures in Chi-town! <br></br>
-                    Not sure what foods to try? Where to explore? Or the best neighborhoods to hang out at? Well here on our website you have access to all that information.
+                <h2 class="animated infinite bounce delay-2s"> About <hr /></h2>
+                <p>Welcome to Odyssey! Doesn't matter where you're coming from or where you're going, our mission is to help you with your adventures in Chi-town! <br />
+                    Not sure what foods to try? Where to explore? Or the best neighborhoods to hang out at? Well here on our website you'll have access to that information.
                     </p> 
             </section>
             <div class="img2">
@@ -25,12 +24,11 @@ function Parallax() {
                 </div>
             </div>
             <section class="section section-light">
-                <h2> Our Services </h2>
-                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores odit incidunt minima recusandae beatae
-                    iure reprehenderit dolores, voluptas itaque nemo, facilis excepturi dicta vitae dolorem, ipsam reiciendis
-            dolore explicabo quis.</p>
+                <h2> What We Offer <hr /></h2>
+                <p> On our website you have access to the best foods and experiences to try here in Chicago. We also have a gallery that's created using a social media API <br />
+                that allows you to explore the most recent posts from the city's residents and visitors. If you're here from a different country, we provide locations of enclaves <br />
+                and languages spoken in Chicago with statistics to help you feel at home. </p>
             </section>
-
             <div class="img3">
                 <div class="ptext">
                     <span class="">
@@ -38,10 +36,7 @@ function Parallax() {
                 </div>
             </div>
 
-
-
         </>
-
     );
 }
 
---
+++ client/src/components/Registration/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./registration.css";
+
+function Registration() {
+    return (
+      <>
+      <nav className="navbar navbar-light">
+        <a className="navbar-brand" href="/">
+          ODYSSEY
+        </a>
+      </nav>
+      <div className="background">
+      </div>
+      </>
+  );
+}
+
+export default Registration;

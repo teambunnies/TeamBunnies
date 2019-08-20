18bbc8cd3f1c6c736be2d69e45c8ab6baef6b5a3
Stephen Malito <stevemalito@gmail.com>
2019-08-20 12:05:57 -0500

    routes and model for neighborhoods

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const userRoutes = require("./users");
 const authRoutes = require("./auth");
 const cnRoutes = require("./chicagoname");
+const neighborhoodRoutes = require("./neighborhood");
 // User routes
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
 router.use("/chicagoname", cnRoutes);
+router.use("/neighborhood", neighborhoodRoutes);
 module.exports = router;

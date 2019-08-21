70506c52655b243ffb17c26d82a89ff218437b62
Stephen Malito <stevemalito@gmail.com>
2019-08-21 12:38:02 -0500

    heatmap working, hardcoded language

--- client/src/components/map/Heatmap.js
+++ client/src/components/map/Heatmap.js
@@ -1,16 +1,18 @@
 import React, { Component, Fragment } from 'react';
 import GoogleMapReact from 'google-map-react';
 import CHICAGO_CENTER from './chicago_center';
+import Select from 'react-select';
 
 class Heatmap extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      places: [
-        
-      ],
-      census: []
+      places: [],
+      census: [],
+      languages: [],
+      selectedOption: null,
+      
     };
   }
 
@@ -21,19 +23,42 @@ class Heatmap extends Component {
       fetch('https://data.cityofchicago.org/resource/a2fk-ec6q.json')
       .then(response => response.json())
       .then(data => this.setState({ census: data }));
+      fetch('languages.json')
+      .then(response => response.json())
+      .then(data => this.setState({ languages:data }));
       
   }
 
+  handleChange = selectedOption => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption.value);
+
+  };
+
   render() {
-    const { places } = this.state;
+    const { places, census, languages, selectedOption, mergedandfiltered } = this.state;
     console.log(this.state)
-    const data = places.map(place => ({
-      lat: place.lat,
-      lng: place.lng,
-      weight: place.weight,
-    }));
+    const options = languages.map(lang =>({
+      value: lang,
+      label: lang,
+   
+    }))
+    const citydata = census.map(cen =>({
+      comm_area: cen.community_area,
+      // I need to bring the .spanish(The Chosen Language in from from selectionOption.value)
+      weight: Math.abs(cen.spanish * .01),
+   
+    }))
+    let weightMap = new Map(citydata.map(({comm_area, weight}) => [comm_area, weight]));
+
+          let res = places.map(
+            ({db_area, lat, lng}) => ({lat, lng, weight: weightMap.get(db_area)})
+          );
+          
+            console.log(res)
+    
     const heatmapData = {
-      positions: data,
+      positions: res,
       options: {
         radius: 20,
         opacity: 2,
@@ -44,7 +69,16 @@ class Heatmap extends Component {
     return (
       <Fragment>
         
+        
         <div className="section" style={{ height: '100vh', width: '100%' }}>
+        <Fragment>
+        <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+
+      />
+      </Fragment>
         <h1>Spanish Speakers</h1>
           <GoogleMapReact
             defaultZoom={10}

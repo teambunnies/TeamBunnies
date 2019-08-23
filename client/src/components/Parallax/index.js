c87b628741a8b13c96f6f0d59014160f3638dd4f
Faraah <ibarra.faraah@gmail.com>
2019-08-23 13:32:24 -0500

    final push

--- client/src/components/Parallax/index.js
+++ client/src/components/Parallax/index.js
@@ -19,7 +19,8 @@ function Parallax() {
             <section class="section section-light-parallax">
                 <h2 class="animated infinite bounce delay-2s"> About <hr /></h2>
                 <p>Welcome to Odyssey! Doesn't matter where you're coming from or where you're going, our mission is to help you with your adventures in Chi-town! <br />
-                    Not sure what foods to try? Where to explore? Or the best neighborhoods to hang out at? Well here on our website you'll have access to that information.
+                    Not sure what foods to try? Where to explore? Or the best neighborhoods to hang out at? Our goal at Odyssey is to help you assimilate to the Chicago culture <br />
+                     by providing you with the best places to check out.
                     </p> 
             </section>
             <div class="img2">
@@ -31,8 +32,8 @@ function Parallax() {
             <section class="section section-light-parallax">
                 <h2> What We Offer <hr /></h2>
                 <p> On our website you have access to the best foods and experiences to try here in Chicago. We also have a gallery that's created using a social media API <br />
-                that allows you to explore the most recent posts from the city's residents and visitors. If you're here from a different country, we provide locations of enclaves <br />
-                and languages spoken in Chicago with statistics to help you feel at home. </p>
+                that allows you to explore the most recent posts from the city's residents and visitors. If you're here from a different country, we provide locations of enclaves, <br />
+                consulates and areas where specific languages are most commonly spoken to help you feel at home. </p>
             </section>
             <div class="img3">
                 <div class="ptext-parallax">
